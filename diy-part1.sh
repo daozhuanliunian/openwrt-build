86ab415bb500367ed2b1133213f8b91de3395611
daozhuanliunian <55530936+daozhuanliunian@users.noreply.github.com>
2021-11-04 11:41:00 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,5 +13,4 @@
 
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-# cd package/lean
-# git clone https://github.com/r1172464137/luci-theme-edge.git
+git clone https://github.com/r1172464137/luci-theme-edge.git package/lean/luci-theme-edge
