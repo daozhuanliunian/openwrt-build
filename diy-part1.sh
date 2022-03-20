fc499f253734a0af5336946d32eb19939f3da869
daozhuanliunian <55530936+daozhuanliunian@users.noreply.github.com>
2022-03-20 20:55:06 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,7 +15,7 @@
 
 # Add a feed source
  echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
- echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+# echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
 #添加主题
 cd package/lean
